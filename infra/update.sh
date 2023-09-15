f97aaa128597d5f97aa5ed5a4922b00572b0f417
Andrey Pozolotin <info@kubestaff.com>
2023-09-16 00:36:45 +0200

    Fixing startup

--- infra/update.sh
+++ infra/update.sh
@@ -1,2 +1,3 @@
-docker compose -f infra/docker-compose.yaml up --build golearn
+docker compose -f infra/docker-compose.yaml stop golearn
+docker compose -f infra/docker-compose.yaml up --build golearn -d
 docker compose -f infra/docker-compose.yaml up -d --remove-orphans
