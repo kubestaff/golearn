b783de1ecaed21b88f1442bace1680549c742a11
Deborah Okangi <deborahokangi@192.168.1.134>
2023-09-22 15:38:15 +0100

    updated main.go

--- main.go
+++ main.go
@@ -1,35 +1,36 @@
 package main
 
 import (
-	"github.com/kubestaff/golearn/user"
-	"github.com/kubestaff/web-helper/server"
+"github.com/kubestaff/golearn/user"
+"github.com/kubestaff/web-helper/server"
 )
 
 func main() {
-	opts := server.Options{Port: 4567}
-	// we create the simplified web server
-	s := server.NewServer(opts)
+opts := server.Options{Port: 4567}
+// we create the simplified web server
+s := server.NewServer(opts)
 
-	// we close the server at the end
-	defer s.Stop()
+// we close the server at the end
+defer s.Stop()
 
-	userProvider := user.Provider{}
+userProvider := user.Provider{}
 
-	user, err := userProvider.GetCurrentUser()
-	if err != nil {
-		variables := map[string]string{
-			"%errorMessage%": err.Error(),
-		}
-		s.PrintFile("error.html", variables)
-	} else {
-		variables := map[string]string{
-			"%name%":            user.GetFullName(),
-			"%profile-picture%": user.GetProfilePic(),
-			"%profileColor%":    user.GetProfileColor(),
-		}
-		s.PrintFile("index.html", variables)
+user, err := userProvider.GetCurrentUser()
+if err != nil {
+	variables := map[string]string{
+		"%errorMessage%": err.Error(),
 	}
-
-	// we start the webserver don't put any code after it
-	s.Start()
+	s.PrintFile("/errors","error.html", variables)
+} else {
+	variables := map[string]string{
+		"%name%":            user.GetFullName(),
+		"%profile-picture%": user.GetProfilePic(),
+		"%profileColor%":    user.GetProfileColor(),
+	}
+	s.PrintFile("/", "index.html", variables)
 }
+
+// we start the webserver don't put any code after it
+s.Start()
+}
+
