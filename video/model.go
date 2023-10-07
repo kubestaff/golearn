88e458a27c5855cb4075125bce185b5efe02ea97
tarequl-haque <tarequlhaque1@gmail.com>
2023-10-07 14:49:56 +0100

    Initial changes 2

---
+++ video/model.go
@@ -0,0 +1,13 @@
+package video
+
+type Video struct {
+	AuthorId string
+	VideoFilePath string
+	ImageFilePath string
+	Title string
+	Description string
+	CreationTime string 
+	Format string
+	IsActive bool
+	DurationSeconds int
+}
