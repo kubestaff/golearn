3497a1676c6bb697903abc69468963f7c849df7c
Tarequl Haque <tarequlhaque1@gmail.com>
2023-10-11 16:40:38 +0100

    merge

--- static/js/main.js
+++ static/js/main.js
@@ -1,74 +1,55 @@
-document.addEventListener("DOMContentLoaded", function(){
-    fetch("/videos")
-        .then(response => response.json())
-        .then(data => {
-            const videosElement = document.getElementById('videos-list')
-            for (var i = 0; i < data.length; i++) {
-                var video = data[i]
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("/videos")
+    .then((response) => response.json())
+    .then((data) => {
+      const videosElement = document.getElementById("videos-list");
+      for (var i = 0; i < data.length; i++) {
+        var video = data[i];
 
-                const videoTemplate = document.getElementById("video-item")
-                const videoItem = videoTemplate.content.cloneNode(true)
+        const videoTemplate = document.getElementById("video-item");
+        const videoItem = videoTemplate.content.cloneNode(true);
 
-                const img = videoItem.querySelector("img")
-                img.src = video.ImageFilePath
+        const img = videoItem.querySelector("img");
+        img.src = video.ImageFilePath;
 
-                const cardText = videoItem.querySelector(".card-text")
-                cardText.textContent = video.Description
-                
-                const durationBlock = videoItem.querySelector(".video-duration")
-                durationBlock.textContent = getDurationText(video.DurationSeconds)
+        const cardText = videoItem.querySelector(".card-text");
+        cardText.textContent = video.Description;
 
-                videosElement.appendChild(videoItem)
-            }
-        }).catch(error => {
-            console.error(error)
-        })
+        const durationBlock = videoItem.querySelector(".video-duration");
+        durationBlock.textContent = getDurationText(video.DurationSeconds);
 
-        let videosLink = document.getElementById("videos-link")
-        videosLink.addEventListener("click", onVideosLinkClick)
+        videosElement.appendChild(videoItem);
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 
-        let settingsLink = document.getElementById("settings-link")
-        settingsLink.addEventListener("click", onSettingsLinkClick)
-})
+  let videosLink = document.getElementById("videos-link");
+  videosLink.addEventListener("click", onVideosLinkClick);
 
+  let settingsLink = document.getElementById("settings-link");
+  settingsLink.addEventListener("click", onSettingsLinkClick);
 
+  let saveSettingsButton = document.getElementById("save-settings-button");
+  saveSettingsButton.addEventListener("click", writeSettings);
+});
 
-    function refreshImages() { 
-        const imageElements = document.querySelectorAll('#videos-list img');
+function onVideosLinkClick() {
+    let videosListContainer = document.getElementById("videos-list-container")
+    videosListContainer.classList.remove("d-none")
 
-        // Iterate through each image element and update its source attribute
-        imageElements.forEach((imgElement) => {
-            // Iterates over all images (using the for.each) and generates a random image URL using picsum.photos.  https://www.w3schools.com/js/js_callback.asp
-            const randomImageUrl = `https://picsum.photos/${imgElement.width}/${imgElement.height}?random=${Math.random()}`; 
+    let settingsListContainer = document.getElementById("settings-list-container")
+    settingsListContainer.classList.add("d-none")
+}
 
-            // math random used to generate new pic every time - https://www.w3schools.com/js/js_random.asp
-            
-            // Set the new image source. .src is short for source from url https://www.w3schools.com/jsref/prop_script_src.asp
-            imgElement.src = randomImageUrl;
-        });
-    }
+function onSettingsLinkClick() {
+    let videosListContainer = document.getElementById("videos-list-container")
+    videosListContainer.classList.add("d-none")
 
-    
-    refreshImages();  
-
-    function onVideosLinkClick() { 
-        let videosListContainer = document.getElementById("videos-list-container") //document.get used to get HTML element from the video list container 
-        videosListContainer.classList.remove("d-none") // d none used to hide element 
-
-        let settingsListContainer = document.getElementById("settings-list-container") 
-        settingsListContainer.classList.add("d-none")
-
-        
-        refreshImages(); // refreshes images when button is pressed 
-    }
-
-    function onSettingsLinkClick() {
-        let videosListContainer = document.getElementById("videos-list-container")
-        videosListContainer.classList.add("d-none")
-
-        let settingsListContainer = document.getElementById("settings-list-container")
-        settingsListContainer.classList.remove("d-none")
-    }
+    let settingsListContainer = document.getElementById("settings-list-container")
+    settingsListContainer.classList.remove("d-none")
+}
 
     
     const refreshButton = document.getElementById("main-call-button"); // main-call-button is the button identifier 
@@ -77,32 +58,31 @@ document.addEventListener("DOMContentLoaded", function(){
     refreshButton.addEventListener("click", refreshImages); // button that calls the refreshImages function
 
 function getDurationText(durationSeconds) {
-    const secondsInMinute = 60
-    let secondText = "second"
+  const secondsInMinute = 60;
+  let secondText = "second";
 
-    if (durationSeconds == 0) {
-        return ""
-    }
-    if (durationSeconds == 1) {
-        return joinText([durationSeconds, secondText])
-    }
+  if (durationSeconds == 0) {
+    return "";
+  }
+  if (durationSeconds == 1) {
+    return joinText([durationSeconds, secondText]);
+  }
 
-    if (durationSeconds < secondsInMinute) {
-        secondText += "s"
-        return joinText([durationSeconds, secondText])
-    }
+  if (durationSeconds < secondsInMinute) {
+    secondText += "s";
+    return joinText([durationSeconds, secondText]);
+  }
 
-    let minuteText = "minute"
-    const durationMinutes = Math.floor(durationSeconds / 60)
-    if (durationMinutes == 1) {
-        return joinText([durationMinutes, minuteText])
-    }
+  let minuteText = "minute";
+  const durationMinutes = Math.floor(durationSeconds / 60);
+  if (durationMinutes == 1) {
+    return joinText([durationMinutes, minuteText]);
+  }
 
-    minuteText +="s"
-    return joinText([durationMinutes, minuteText])
+  minuteText += "s";
+  return joinText([durationMinutes, minuteText]);
 }
 
 function joinText(textItems) {
-    return textItems.join(" ");
+  return textItems.join(" ");
 }
-
